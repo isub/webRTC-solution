51cddd406291680b1ca557a20b5b2365c11cd829
Sabir <subbeer@gmail.com>
2021-03-24 16:35:55 +0300

    Update device_info.js

--- src/wrtc/js/device_info.js
+++ src/wrtc/js/device_info.js
@@ -1,16 +1,5 @@
 
-function wrtc_di_enumDevices( withVideo ) {
-	try {
-		let constraints = { 'video': true, 'audio': true };
-		if( ! withVideo ) {
-			constraints = { 'video': false, 'audio': true };
-		}
-		const stream = openMediaDevices( constraints );
-		console.log( 'Got MediaStream:', stream );
-	} catch( error ) {
-		console.error( 'Error accessing media devices.', error );
-	}
-}
-const openMediaDevices = async( constraints ) => {
-	return await navigator.mediaDevices.getUserMedia( constraints );
+function wrtc_di_enumDevices() {
+	let devices = navigator.mediaDevices.enumerateDevices();
+	return devices;
 }
