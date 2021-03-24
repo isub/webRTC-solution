41b228afd846727b0f5b76df4ff6bba4a8e80319
Sabir <subbeer@gmail.com>
2021-03-24 14:49:03 +0300

    1

---
+++ src/wrtc/js/device_info.js
@@ -0,0 +1,13 @@
+function wrtc_di_enumDevices() {
+	const constraints = {
+		'video': true,
+		'audio': true
+	}
+	navigator.mediaDevices.getUserMedia(constraints)
+		.then( stream => {
+			console.log( 'Got MediaStream:', stream );
+		} )
+		.catch( error => {
+			console.error( 'Error accessing media devices.', error );
+		} );
+}
---
+++ src/wrtc/js/main.js
@@ -0,0 +1,5 @@
+include device_info.js;
+
+document.onload = function() {
+	wrtc_di_enumDevices();
+}
