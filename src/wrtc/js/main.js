19c364a6fceeeedf167d2ff6b7c3d95cf66d671e
Sabir <subbeer@gmail.com>
2021-03-24 16:02:30 +0300

    Update main.js

--- src/wrtc/js/main.js
+++ src/wrtc/js/main.js
@@ -2,12 +2,12 @@
 	function () {
 		document.getElementById('login-btn').addEventListener( 'click', doLogin );
 		document.getElementById('enum-devices-btn').addEventListener( 'click', enumDevices );
-	}
-	function doLogin() {
-		console.log( 'in function doLogin' );
-	}
-	function enumDevices() {
-		console.log( 'in function enumDevices' );
-		wrtc_di_enumDevices( document.getElementById( 'with-video' ).value );
+		function doLogin() {
+			console.log( 'in function doLogin' );
+		}
+		function enumDevices() {
+			console.log( 'in function enumDevices' );
+			wrtc_di_enumDevices( document.getElementById( 'with-video' ).value );
+		}
 	}
 )();
