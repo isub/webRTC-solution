7aa0cc0805778f8d3c5143e98438d0f67cd7f269
Sabir <subbeer@gmail.com>
2021-03-24 15:06:13 +0300

    3

--- src/wrtc/index.php
+++ src/wrtc/index.php
@@ -32,6 +32,7 @@
 			?>
 		</div>
 
+		<script type="text/javascript" src="js/device_info.js"></script>
 		<script type="text/javascript" src="js/main.js"></script>
 	</body>
 </html>
--- src/wrtc/js/main.js
+++ src/wrtc/js/main.js
@@ -1,4 +1,3 @@
-include device_info.js;
 
 document.onload = function() {
 	wrtc_di_enumDevices();
