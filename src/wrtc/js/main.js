8c31618b84e93c9656bd4f747b013f3444df0f8b
Sabir <subbeer@gmail.com>
2021-03-24 16:01:31 +0300

    1

--- src/wrtc/js/main.js
+++ src/wrtc/js/main.js
@@ -4,8 +4,10 @@
 		document.getElementById('enum-devices-btn').addEventListener( 'click', enumDevices );
 	}
 	function doLogin() {
+		console.log( 'in function doLogin' );
 	}
 	function enumDevices() {
+		console.log( 'in function enumDevices' );
 		wrtc_di_enumDevices( document.getElementById( 'with-video' ).value );
 	}
 )();
