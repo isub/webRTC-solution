66354a0efc30ea3a5af42869841f82263395b2ca
Sabir <subbeer@gmail.com>
2021-03-25 13:06:44 +0300

    Update main.js

--- src/wrtc/js/main.js
+++ src/wrtc/js/main.js
@@ -3,10 +3,8 @@
 		document.addEventListener( "DOMContentLoaded", docLoaded );
 		document.getElementById( 'login-btn' ).addEventListener( 'click', doLogin );
 		document.getElementById( 'settings-btn' ).addEventListener( 'click', openSettings );
-		function docLoaded() {
-			let devices = wrtc_di_enumDevices();
-			console.debug( 'wrtc_di_enumDevices() result: ', devices );
-			let filtered = devices.filter( device => device.kind === "audioinput" );
+		function fillSelect( elementId, devices, deviceType ) {
+			let filtered = devices.filter( device => device.kind === deviceType );
 			console.debug( 'filter result: ', filtered );
 			let txtHTML;
 			filtered.forEarch(
@@ -14,7 +12,12 @@
 					txtHTML += '<option>' + item.label + '</option>';
 				}
 			);
-			document.getElementById( 'settings-btn' ).innerHTML = txtHTML;
+			document.getElementById( elementId ).innerHTML = txtHTML;
+		}
+		function docLoaded() {
+			let devices = wrtc_di_enumDevices();
+			console.debug( 'wrtc_di_enumDevices() result: ', devices );
+			fillSelect( 'settings-btn', devices, 'audioinput' );
 		}
 		function doLogin() {
 			console.log( 'in function doLogin' );
