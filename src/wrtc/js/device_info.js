088df6e31d92458642474b3ae9b3d65ad403ac7c
Sabir <subbeer@gmail.com>
2021-03-25 09:38:10 +0300

    Update device_info.js

--- src/wrtc/js/device_info.js
+++ src/wrtc/js/device_info.js
@@ -1,9 +1,9 @@
 
 function wrtc_di_enumDevices() {
-	let devices = navigator.mediaDevices.enumerateDevices();
-	console.debug( 'wrtc_di_enumDevices', devices );
 	let regExp = /[0-9,a-f]/i;
 	var retVal = [];
+	let devices = navigator.mediaDevices.enumerateDevices();
+	console.debug( 'wrtc_di_enumDevices', devices );
 	devices.forEach(
 		function( item, ind, devices ) {
 			if( regExp.test( item.deviceId ) ) {
