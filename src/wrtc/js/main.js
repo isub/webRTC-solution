e963a1d64091291080df452147ca2646f9f09cd2
Sabir <subbeer@gmail.com>
2021-03-29 11:52:10 +0300

    Update main.js

--- src/wrtc/js/main.js
+++ src/wrtc/js/main.js
@@ -45,7 +45,7 @@
 			}
 		}
 		function makeCall() {
-			const configuration = { 'iceServers': [ { 'urls': 'turn:sip.dtco.ru' } ] };
+			const configuration = { 'iceServers': [ { 'urls': 'turn:sip.dtco.ru', 'username': 'sip.dtco.ru', 'credential': 'Gh0uy0pG0u0ls' } ] };
 			const peerConnection = new RTCPeerConnection( configuration );
 			console.log( 'local connection description: ', peerConnection.currentLocalDescription );
 		}
