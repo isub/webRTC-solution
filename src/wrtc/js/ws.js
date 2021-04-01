6d2b051a96d885797576a8bf3a400bf5790c5784
Sabir <subbeer@gmail.com>
2021-04-01 13:01:28 +0300

    Update ws.js

--- src/wrtc/js/ws.js
+++ src/wrtc/js/ws.js
@@ -1,25 +1,26 @@
 
 var wssUrl = 'ws:sip.dtco.ru:8081';
-var webSock = new WebSocket;
+var webSock;
 var onMessage_cb;
 
 function wrtc_ws_init() {
-}
-webSock.onopen = function() {
-	console.debug( 'web socket connection is established successfully' );
-}
-webSock.onclose = function( event ) {
-	if( event.wasClean ) {
-		console.debug( 'web socket connection is closed gracefully' );
-	} else {
-		console.debug( 'web socket connection failed: ', event.reason );
+	webSock = new WebSocket( wssUrl );
+	webSock.onopen = function() {
+		console.debug( 'web socket connection is established successfully' );
 	}
-}
-webSock.onmessage = function( event ) {
-	onMessage_cb( event );
-}
-webSock.onerror = function( event ) {
-	console.log( 'an error occurred on web socket: ', event );
+	webSock.onclose = function( event ) {
+		if( event.wasClean ) {
+			console.debug( 'web socket connection is closed gracefully' );
+		} else {
+			console.debug( 'web socket connection failed: ', event.reason );
+		}
+	}
+	webSock.onmessage = function( event ) {
+		onMessage_cb( event );
+	}
+	webSock.onerror = function( event ) {
+		console.log( 'an error occurred on web socket: ', event );
+	}
 }
 function wrtc_ws_sendMessage( message, callback ) {
 	onMessage_cb = callback;
