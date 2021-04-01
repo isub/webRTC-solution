fe70a180c8911a520334be806b03964f797c5482
Sabir <subbeer@gmail.com>
2021-04-01 12:56:32 +0300

    Update device_info.js

--- src/wrtc/js/device_info.js
+++ src/wrtc/js/device_info.js
@@ -1,5 +1,9 @@
 
 async function wrtc_di_enumDevices() {
+	if( navigator.mediaDevices.enumerateDevices !== 'undefined' ) {
+	} else {
+		return;
+	}
 	let regExp = /^[0-9,a-f]+$/i;
 	var retVal = [];
 	let devices = await navigator.mediaDevices.enumerateDevices();
