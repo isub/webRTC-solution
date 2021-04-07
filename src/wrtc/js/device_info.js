eda104d704cf626b73294237ccc1ba827188feee
Sabir <subbeer@gmail.com>
2021-04-07 10:38:23 +0300

    Update device_info.js

--- src/wrtc/js/device_info.js
+++ src/wrtc/js/device_info.js
@@ -1,17 +1,21 @@
+var isUsingAudio = true;
+var isUsingVideo = true;
 
 async function wrtc_di_enumDevices() {
-	var retVal = [];
+	let retVal = [];
 	if( navigator.mediaDevices === undefined ) {
 		return retVal;
 	} else {
 	}
 	try {
-		await navigator.mediaDevices.getUserMedia( { audio: true, video: false } );   
+		await navigator.mediaDevices.getUserMedia( { audio: isUsingAudio, video: isUsingVideo } );   
 	} cathc( err ) {
+		isUsingAudio = false;
 	}
 	try {
 		await navigator.mediaDevices.getUserMedia( { audio: false, video: true } );   
 	} cathc( err ) {
+		isUsingVideo = false;
 	}
 	let deviceList = await navigator.mediaDevices.enumerateDevices();
 	let regExp = /^[0-9,a-f]+$/gi;
