98678ca688029652977082747a533e3ccc280164
Sabir <subbeer@gmail.com>
2021-04-07 09:29:11 +0300

    Update device_info.js

--- src/wrtc/js/device_info.js
+++ src/wrtc/js/device_info.js
@@ -1,6 +1,6 @@
 
 async function wrtc_di_enumDevices() {
-//	let regExp = /^[0-9,a-f]+$/i;
+	let regExp = /^[0-9,a-f]+$/gi;
 	var retVal = [];
 	if( navigator.mediaDevices === undefined ) {
 		return retVal;
@@ -9,7 +9,7 @@ async function wrtc_di_enumDevices() {
 	let devices = await navigator.mediaDevices.enumerateDevices();
 	devices.forEach(
 		function( item, ind, devices ) {
-//			if( regExp.test( item.deviceId ) ) {
+			if( regExp.test( item.deviceId ) ) {
 				retVal.push(
 					{
 						kind: item.kind,
@@ -17,7 +17,7 @@ async function wrtc_di_enumDevices() {
 						label: item.label
 					}
 				);
-//			}
+			}
 		}
 	);
 	return retVal;
